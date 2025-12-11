350401bcf7aac2947e946c02123068ba9ce6d07c
Akash rathour <113913977+odetteswaan@users.noreply.github.com>
2025-12-11 14:35:30 +0530

    changes

--- Frontendpart/src/EmployeeDashboard/Endpoint .js
+++ Frontendpart/src/EmployeeDashboard/Endpoint .js
@@ -1,4 +1,4 @@
-export const BaseUrl="http://127.0.0.1:5000"
+export const BaseUrl="https://employeedashboardapi.onrender.com/"
 export const endpoint="/employees"
 export const SigninEndpoint="/employee/signin"
 export const SignupEndpoint="/employee/signup"
